e3a797d5488c9ba7019e67c39084047a8566280f
gg <labankiplagat81@gmail.com>
2020-04-06 23:43:05 +0300

    templates.

--- django_project/ecommerce_app/urls.py
+++ django_project/ecommerce_app/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
         views.show_product, name='product_detail'),
     path('cart/', views.show_cart, name='show_cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('process-payment/', views.process_payment, name='process_payment'),
+    path('payment-done/', views.payment_done, name='payment_done'),
+    path('payment-cancelled/', views.payment_canceled, name='payment_cancelled'),
+
+
 ]
